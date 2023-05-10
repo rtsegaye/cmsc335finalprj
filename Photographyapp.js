b785fd40e470e1a7a5a64bf74f1f51bb99369af9
Emmanuel Kebede <emmanuelemrias@gmail.com>
2023-05-10 10:34:10 -0400

    commit

--- Photographyapp.js
+++ Photographyapp.js
@@ -6,14 +6,13 @@ const app = express();
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//db
-const databaseAndCollection = {db: "CMSC335FINALDB", collection:"reviewSession"};
-const userName = process.env.MONGO_DB_USERNAME;
-const password = process.env.MONGO_DB_PASSWORD;
+
+
 
 
 
@@ -46,7 +45,22 @@ const renderFile = (path, res) => {
       res.send(html);
     });
   };
-  
+
+
+
+  // Connect to Mongo
+//db
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+const dbname = process.env.MONGO_DB_NAME;
+const emailPass = process.env.EMAIL_PASS
+const collection = process.env.MONGO_COLLECTION ;
+const databaseAndCollection = {db: `${dbname}`, collection:`${collection}`};
+
+const uri = `mongodb+srv://${userName}:${password}@cluster0.ux1fzck.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect();
+
 app.get('/', (req, res) => {
     renderFile('./templates/index.ejs', res);
 });
@@ -54,6 +68,38 @@ app.get('/', (req, res) => {
 app.get('/appointment', (req, res) => {
     renderFile('./templates/appointment.ejs', res);
 });
+
+app.post('/appointment', async (req, res) => {
+  nameVal = req.body.name
+  email = req.body.email
+  Phone = "" + req.body.phone1 + "-"+ req.body.phone2 + "-" +req.body.phone3
+  dateVal = req.body.date
+  session = req.body.session
+  id = undefined
+  message = "For some reason, this appointment was not made. "
+
+  console.log(nameVal, email, Phone, dateVal, session)
+
+  try {
+    await client.connect();
+    let person = {name : nameVal, email : email, phone: Phone, date: dateVal, session : session}
+    const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(person);
+    sendConfirmation(nameVal, email, dateVal)
+    id = result.insertedId
+    console.log(`Person entered with id ${result.insertedId}`);
+  } catch (e){
+    console.error(e)
+  } finally {
+    await client.close();
+  }
+  
+  if(id){
+    Application = `Thank you very much. Please check your email for a confirmation`
+  }
+
+  res.send(Application);
+})
+
 app.get('/prices', (req, res) => {
     renderFile('./templates/prices.ejs', res);
 });
@@ -85,8 +131,7 @@ app.post('/submitreview', async (req, res) => {
     });
 
 async function insertApplicationData(applicationData) {
-    const uri = `mongodb+srv://${userName}:${password}@cluster0.byxwry2.mongodb.net/?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
     try {
         await client.connect();
         await insertreview(client, databaseAndCollection, applicationData);
@@ -100,6 +145,31 @@ async function insertApplicationData(applicationData) {
 
 async function insertreview(client, databaseAndCollection, newReview) {
     const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(newReview);
+}
+
+function sendConfirmation(name, email, date){
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'emmanuelermias@gmail.com',
+      pass: emailPass
+    }
+  });
+
+  var mailOptions = {
+    from: 'ekebede2@terpmail.umd.edu',
+    to: email,
+    subject: 'Appointment cofirmation',
+    text: `Greetings ${name}, \n\n This is a confirmation message for your appointment on ${date}, with AfroFlick`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
   
